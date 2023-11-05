c17a677703819ad345eaddfd4ff3d02d12e693ba
Pranit <pranit.kittad@gmail.com>
2023-11-06 01:38:08 +0530

    added daa

---
+++ DAA/daa1.py
@@ -0,0 +1,39 @@
+def Fibonacci(n):
+
+	# Check if input is 0 then it will
+	# print incorrect input
+	if n < 0:
+		print("Incorrect input")
+
+	# Check if n is 0
+	# then it will return 0
+	elif n == 0:
+		return 0
+
+	# Check if n is 1,2
+	# it will return 1
+	elif n == 1 or n == 2:
+		return 1
+
+	else:
+		t1 = Fibonacci(n-1)
+		t2 = Fibonacci(n-2)
+		return t1+t2
+
+
+# Driver Program
+print(Fibonacci(9))
+
+
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2 
+count = 1
+
+while count <= n:
+	print(next_number, end=" ")
+	count += 1
+	num1, num2 = num2, next_number
+	next_number = num1 + num2
+print()
